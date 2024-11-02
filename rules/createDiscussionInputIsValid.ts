ea80a2cbc3660bd152d479ce8204543a06ae9e05
Catherine Luse <gennitdev@gmail.com>
2024-11-02 13:58:32 -0700

    Enforce discussion title character limit

--- rules/createDiscussionInputIsValid.ts
+++ rules/createDiscussionInputIsValid.ts
@@ -1,6 +1,6 @@
 import { rule } from "graphql-shield";
 import { CanCreateDiscussionArgs } from "./rules";
-import { MAX_CHARS_IN_DISCUSSION_BODY } from "./updateDiscussionInputIsValid";
+import { MAX_CHARS_IN_DISCUSSION_BODY, MAX_CHARS_IN_DISCUSSION_TITLE } from "./updateDiscussionInputIsValid";
 
 export const createDiscussionInputIsValid = rule({ cache: "contextual" })(
   async (parent: any, args: CanCreateDiscussionArgs, ctx: any, info: any) => {
@@ -14,6 +14,10 @@ export const createDiscussionInputIsValid = rule({ cache: "contextual" })(
     if (!title) {
       return "A title is required.";
     }
+
+    if (title && title.length > MAX_CHARS_IN_DISCUSSION_TITLE) {
+        return `The discussion title cannot exceed ${MAX_CHARS_IN_DISCUSSION_TITLE} characters.`;
+      }
 
     if (body && body.length > MAX_CHARS_IN_DISCUSSION_BODY) {
       return "The discussion body cannot exceed 18,000 characters.";
